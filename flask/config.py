d457ed0dc2514e32ad2ec43f551235c56a065bbb
lwy1471 <honeybees@naver.com>
2018-04-24 00:59:45 +0900

    Delete config.py

--- flask/config.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
-##### Conversation Service Credentials info #####
-conversation_endpoint = "https://gateway.aibril-watson.kr/conversation/api"
-conversation_username = "57b4553d-929e-4217-bb78-5fbcff381e04"
-conversation_password = "JpBCh5hc6s2E"
-workspace_main = "7a0e055d-a9fd-4697-b167-b05241fac618"
